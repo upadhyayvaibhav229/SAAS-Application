cc5064df2baf7adb710e55c36cb262fda9e3ad6d
upadhyayvaibhav229 <upadhyayvaibhav229@gmail.com>
2025-05-19 16:06:44 +0530

    fdgdfsgsg

--- Frontend/src/Component/EmailVerified.jsx
+++ Frontend/src/Component/EmailVerified.jsx
@@ -10,13 +10,26 @@ const EmailVerified = () => {
             inputRef.current[index - 1].focus();
         }
     }
+
+    const handleKeyDown = () => {
+      if (e.key === 'Backspace' && e.target.value === '' && index > 0) {
+        inputRef.current[index - 1].focus();
+      }
+    }
+
+    const handlePaste = () => {
+      const values = e.clipboardData.getData('Text').split('');
+      values.forEach((value, index) => {
+        inputRef.current[index].value = value;
+      });
+    }
   return (
     <div className='flex flex-col items-center justify-center h-screen bg-gradient-to-r from-blue-500 to-purple-500 text-white '>
       <form action="" className="">
         <div className="bg-[#333A5C] text-white p-6 rounded-lg shadow-lg">
           <h1 className="text-2xl font-bold mb-4">Email Verified</h1>
           <p className="mb-4">Enter the 6 digit code sent to your email</p>
-            <div className="mb-4">
+            <div className="mb-4" onPaste={handlePaste}>
                 {
                     Array.from({ length: 6 }, (_, index) => (
                         <input
@@ -26,6 +39,8 @@ const EmailVerified = () => {
                             className="w-12 h-12 text-center border border-gray-300 rounded mx-1 focus:outline-none focus:ring-2 focus:ring-blue-500 bg-[#141f53]"
                             ref={(el) => (inputRef.current[index] = el)}
                             onChange={(e) => handleInput(e, index)}
+                            onKeyDown={(e) => handleKeyDown(e, index)}
+
                             onFocus={(e) => e.target.select()} 
                         />
                     ))
